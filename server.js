58b79bbe75d9035d0383a257f33ea6d464d84399
Deepak Sharma <dasjideepak@gmail.com>
2026-07-09 03:55:01 +0530

    Sync dataset v1.2.0 (prefix cleanup + card URLs)

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ function errorText(message) {
   return { content: [{ type: 'text', text: message }], isError: true };
 }
 
-const server = new McpServer({ name: 'tarotoo-tarot', version: '1.1.0' });
+const server = new McpServer({ name: 'tarotoo-tarot', version: '1.2.0' });
 
 server.registerTool(
   'get_card_meaning',
